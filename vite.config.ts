53101905ed2566b9f2ba945e394b4aa4ac69c0c9
Jonathan Cancelli <jonathan.cancelli.46@gmail.com>
2026-08-28 11:19:09 +0200

    config dev and prod environments' paths

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter(),
+			paths: {
+				base: import.meta.env.DEV ? "" : "/japanese-vocabulary",
+			},
 		}),
 	],
 })
