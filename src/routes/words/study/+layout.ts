2d5793f0e8dd2c25ea8380967bad967202c4d9a9
Jonathan Cancelli <jonathan.cancelli.46@gmail.com>
2026-09-01 22:29:28 +0200

    fix import

--- src/routes/words/study/+layout.ts
+++ src/routes/words/study/+layout.ts
@@ -1,12 +1,12 @@
 import type { WordDTO } from "$lib/dto.svelte"
 import { storage } from "$lib/local_storage"
-import { WordStudySessionStep } from "$lib/study_session"
+import { StudySessionStep } from "$lib/study_session"
 import type { LayoutLoad } from "./$types"
 
 export const load: LayoutLoad = async () => {
 	return {
 		params: storage.wordStudySession.params,
 		words: [] as WordDTO[],
-		step: WordStudySessionStep.CONFIGURE,
+		step: StudySessionStep.CONFIGURE,
 	}
 }
