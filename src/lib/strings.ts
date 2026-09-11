494ba29ca737544898f51eddfd99b2273ee39098
Jonathan Cancelli <jonathan.cancelli.46@gmail.com>
2026-09-11 17:23:48 +0200

    update strings

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -1,4 +1,4 @@
-import { AdjectiveType, JLPTLevel, VerbType, Difficulty, WordType } from "./model"
+import { AdjectiveType, ItemType, JLPTLevel, VerbType, WordType } from "./model"
 import { StudySessionLanguage } from "./study_session"
 
 export const JLPT_LEVEL_PRETTY_STRING = {
@@ -9,20 +9,16 @@ export const JLPT_LEVEL_PRETTY_STRING = {
 	[JLPTLevel.N5]: "N5",
 } as const
 
-export const DIFFICULTY_PRETTY_STRING = {
-	[Difficulty.DONT_KNOW]: "Don't know",
-	[Difficulty.KINDA_DONT_KNOW]: "Kinda don't know",
-	[Difficulty.KINDA_KNOW]: "Kinda know",
-	[Difficulty.KNOW]: "Know",
-	[Difficulty.UNFORGETTABLE]: "Unforgettable",
+export const ITEM_TYPE_PRETTY_STRING = {
+	[ItemType.WORD]: "Word",
+	[ItemType.KANJI]: "Kanji",
+	[ItemType.COUNTER]: "Counter",
 } as const
 
 export const WORD_TYPE_PRETTY_STRING = {
-	[WordType.NOUN]: "Noun",
+	[WordType.SIMPLE]: "Simple word",
 	[WordType.VERB]: "Verb",
-	[WordType.ADVERB]: "Adverb",
 	[WordType.ADJECTIVE]: "Adjective",
-	[WordType.PRE_NOUN_ADJECTIVAL]: "Pre-noun adjectival",
 } as const
 
 export const VERB_TYPE_PRETTY_STRING = {
